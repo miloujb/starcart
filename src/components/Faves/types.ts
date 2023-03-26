1fe88dbbc8a922fbf05b9162d673a1ee71f2e3a4
miloujb <emilyjbailey@hotmail.co.uk>
2023-03-26 14:01:48 +0100

    changes to obj

--- src/components/Faves/types.ts
+++ src/components/Faves/types.ts
@@ -2,6 +2,8 @@ import {SyntheticEvent} from 'react'
 import {ButtonProps, RatingProps} from 'semantic-ui-react'
 export interface FaveItemObj{
     id:number;
+    name:string;
+    rating: number;
 }
 export type handleRemove = (_e: SyntheticEvent, data: ButtonProps) => void
 export type handleRating = (_e: SyntheticEvent, data: RatingProps) => void
