7ae431fb5b8f38039f4bac6c4489f70e9fdcc7f1
miloujb <emilyjbailey@hotmail.co.uk>
2023-03-26 15:07:22 +0100

    handle remove function tested

--- src/components/Faves/FaveItem.test.js
+++ src/components/Faves/FaveItem.test.js
@@ -1,6 +1,14 @@
 import FaveItem
 	from './FaveItem'
-import {render, screen} from '@testing-library/react'
+import {
+	fireEvent,
+	getByTestId,
+	render,
+	screen,
+} from '@testing-library/react'
+import {
+	handleRating
+} from './types'
 
 const props = {
 	fave: {id: 123, name: "A New Hope", rating: 4}
@@ -10,5 +18,11 @@ describe('Fave Item Tests', () => {
 	it('renders the title on the page', () => {
 		render(<FaveItem {...props} />)
 		expect(screen.getByText(props.fave.name)).toBeInTheDocument();
+	})
+	it("calls add fave on button click", async() => {
+		const handleRemove = jest.fn();
+		render(<FaveItem {...props} handleRating={handleRating} handleRemove={handleRemove} />)
+		fireEvent.click(screen.getByTestId('remove-button'));
+		expect(handleRemove).toHaveBeenCalled();
 	})
 })
--- src/components/Faves/FaveItem.tsx
+++ src/components/Faves/FaveItem.tsx
@@ -13,7 +13,7 @@ const FaveItem = ({ fave, handleRating, handleRemove }:Props)=> (
 	<List.Item>
 		<img className='ui mini circular image' src="avatar.jpg" alt="avatar" />
 		<List.Content>
-			{fave.name} <Button onClick={handleRemove} size="mini" icon={{ name: 'delete', color: 'red' }} basic />
+			{fave.name} <Button onClick={handleRemove} size="mini" data-testid="remove-button" icon={{ name: 'delete', color: 'red' }} basic />
 		</List.Content>
 		<List.Content>
 			<Rating onRate={handleRating} maxRating={5} icon="star" size="mini" rating={fave.rating} />
